9af34bbbdecc12d3aa5d21863dfdc011541dc1f5
HenriPham280100 <phamk883@gmail.com>
2024-02-25 11:51:03 +0700

    first commit

---
+++ src/main/java/chatte/project/v01/CassandraConfig.java
@@ -0,0 +1,62 @@
+package chatte.project.v01;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.cassandra.SessionFactory;
+
+import org.springframework.data.cassandra.config.CqlSessionFactoryBean;
+import org.springframework.data.cassandra.config.SchemaAction;
+import org.springframework.data.cassandra.config.SessionFactoryFactoryBean;
+import org.springframework.data.cassandra.core.CassandraOperations;
+import org.springframework.data.cassandra.core.CassandraTemplate;
+import org.springframework.data.cassandra.core.convert.CassandraConverter;
+import org.springframework.data.cassandra.core.convert.MappingCassandraConverter;
+import org.springframework.data.cassandra.core.mapping.CassandraMappingContext;
+import org.springframework.data.cassandra.core.mapping.SimpleUserTypeResolver;
+import org.springframework.data.cassandra.repository.config.EnableCassandraRepositories;
+
+import com.datastax.oss.driver.api.core.CqlSession;
+
+@Configuration
+@EnableCassandraRepositories(basePackages = { "org.springframework.data" })
+public class CassandraConfig {
+
+    @Bean
+    public CqlSessionFactoryBean session() {
+        CqlSessionFactoryBean sessionFactory = new CqlSessionFactoryBean();
+        sessionFactory.setKeyspaceName("chatte_key");
+        sessionFactory.setLocalDatacenter("DC1");
+        return sessionFactory;
+    }
+
+    @Bean
+    public SessionFactoryFactoryBean sessionFactory(CqlSession session, CassandraConverter converter) {
+
+        SessionFactoryFactoryBean sessionFactory = new SessionFactoryFactoryBean();
+        sessionFactory.setSession(session);
+        sessionFactory.setConverter(converter);
+        sessionFactory.setSchemaAction(SchemaAction.NONE);
+
+        return sessionFactory;
+    }
+
+    @Bean
+    public CassandraMappingContext mappingContext() {
+        return new CassandraMappingContext();
+    }
+
+    @Bean
+    public CassandraConverter converter(CqlSession cqlSession, CassandraMappingContext mappingContext) {
+
+        MappingCassandraConverter cassandraConverter = new MappingCassandraConverter(mappingContext);
+        cassandraConverter.setUserTypeResolver(new SimpleUserTypeResolver(cqlSession));
+
+        return cassandraConverter;
+    }
+
+    @Bean
+    public CassandraOperations cassandraTemplate(SessionFactory sessionFactory, CassandraConverter converter) {
+        return new CassandraTemplate(sessionFactory, converter);
+    }
+
+}
---
+++ src/main/java/chatte/project/v01/RedisConfig.java
@@ -0,0 +1,35 @@
+package chatte.project.v01;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisConfiguration;
+import org.springframework.data.redis.connection.RedisPassword;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+
+
+@Configuration
+@ConfigurationProperties
+public class RedisConfig {
+
+    @Bean
+    @Primary
+    public ReactiveRedisConnectionFactory reactiveRedisConnectionFactory(RedisConfiguration defaultRedisConfig) {
+        LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder()
+                .useSsl().build();
+        return new LettuceConnectionFactory(defaultRedisConfig, clientConfig);
+    }
+
+    @Bean
+    public RedisConfiguration defaultRedisConfig() {
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+        config.setHostName("127.0.0.1");
+        config.setPort(6379);
+        config.setPassword(RedisPassword.of("khoakhoa@123"));
+        return config;
+    }
+}
